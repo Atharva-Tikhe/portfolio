b4595d72038c2ad8c6ff892d23305fa33a81ba5c
Atharva-Tikhe <atharvatikhe22@gmail.com>
2026-02-27 21:01:47 +0000

    fix: removed file reading; now serving from url

--- src/routes/cv/+server.ts
+++ src/routes/cv/+server.ts
@@ -1,16 +1,10 @@
 import type { RequestHandler } from "@sveltejs/kit";
-import { readFile } from "node:fs/promises";
-import { fileURLToPath } from "node:url";
-import {resolve} from 'node:path';
 
 export const GET: RequestHandler = async() => {
-    const path = resolve('static/EMBL-EBI_cv.pdf');
-    const pdf = await readFile(path);
-
-    return new Response(pdf, {
+    return new Response(null, {
+        status: 302,
         headers: {
-            'Content-Type': 'application/pdf',
-            'Content-Disposition': 'inline; filename="atharva_tikhe_cv.pdf"'
+            Location: '/EMBL-EBI_cv.pdf'
         }
-    })
-}
+    });
+};
