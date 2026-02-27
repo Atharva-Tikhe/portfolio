9407d1637cac1858ea8969527f1dfdbd1e3ac103
Atharva-Tikhe <atharvatikhe22@gmail.com>
2026-02-27 20:50:06 +0000

    fix: removed redundant favicon import

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	// import favicon from '$lib/assets/favicon.svg';
 
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<!-- <link rel="icon" href={favicon} /> -->
 	<title>Atharva Tikhe</title>	
 
 </svelte:head>
