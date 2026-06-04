2fc1e3843bef443103bf7bc92fcc169f4e306466
Atharva-Tikhe <atharvatikhe22@gmail.com>
2026-06-04 20:32:39 +0100

    fix: hero rename

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import Hero from '$lib/Hero.svelte';
+    import Hero from '$lib/hero.svelte';
     import Nav from '$lib/Nav.svelte';
     import About from '$lib/About.svelte';
     import Education from '$lib/Education.svelte';
